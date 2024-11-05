74788d6dda5b56a4cc0dc7a605e03244d6101ca9
Kirill S <zuzuwaq@gmail.com>
2024-11-05 17:51:58 +0500

    change openapi auth parameters & add media dir

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from auth.router import router as auth_router
 from user_profile.router import router as profile_router
 from events.router import router as events_router
 from database import async_session_maker
+from fastapi.openapi.utils import get_openapi
 
 
 app = FastAPI()
@@ -13,6 +14,34 @@ app = FastAPI()
 app.include_router(auth_router)
 app.include_router(profile_router)
 app.include_router(events_router)
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Your API",
+        version="1.0.0",
+        description="API for event management",
+        routes=app.routes,
+    )
+    
+    openapi_schema["components"]["securitySchemes"] = {
+        "Bearer": {
+            "type": "http",
+            "scheme": "bearer",
+            "bearerFormat": "JWT",
+        }
+    }
+    
+    for path in openapi_schema["paths"].values():
+        for operation in path.values():
+            operation["security"] = [{"Bearer": []}]
+    
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+app.openapi = custom_openapi
 
 origins = [
     "*",
