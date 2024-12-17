7dbf6ff3963d2bf0cf54f1feec35e29ad6fad535
Kirill S <zuzuwaq@gmail.com>
2024-12-17 16:57:33 +0500

    provide content-length and head method to cors

--- src/main.py
+++ src/main.py
@@ -53,9 +53,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT", "HEAD"],
     allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
-                   "Authorization"],
+                   "Authorization", "Content-Length"],
 )
 
 app.mount("/media", StaticFiles(directory="media"), name="media")
