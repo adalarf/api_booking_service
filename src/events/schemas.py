0149fa3b0126a429596dff932b90dd7e07a718ea
Kirill S <zuzuwaq@gmail.com>
2025-01-10 19:29:37 +0500

    delete times slots and custom field in event

--- src/events/schemas.py
+++ src/events/schemas.py
@@ -74,6 +74,19 @@ class EventUpdateSchema(BaseModel):
         return value
 
 
+class EventDateTimeToDeteleSchema(BaseModel):
+    id: Optional[int] = None
+
+
+class CustomFieldToDeteleSchema(BaseModel):
+    id: Optional[int] = None
+
+
+class EventRegistrationFieldsToDeleteSchema(BaseModel):
+    custom_fields: Optional[List[CustomFieldToDeteleSchema]] = []
+    event_dates_times: Optional[List[EventDateTimeToDeteleSchema]] = []
+
+
 class EventCreateResponseSchema(BaseModel):
     msg: str
     event_id: int
